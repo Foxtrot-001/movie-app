282d9d817ff7d2d541d54bd4f743f75df86231db
Foxtrot-001 <135134943+Foxtrot-001@users.noreply.github.com>
2023-10-27 06:50:29 -0500

    first commit

---
+++ src/contact.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Contact = () => {
+ return (
+    <div>
+      <h2>Contact</h2>
+      <p>+1234567890</p>
+      <p>contacto@ejemplo.com</p>
+    </div>
+ );
+};
+
+export default Contact;
